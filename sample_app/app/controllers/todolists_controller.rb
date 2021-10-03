bfc47689ba266c2640abe1be3260f8f3b4dba65c
EC2 Default User <ec2-user@ip-172-31-16-9.ec2.internal>
2021-10-03 23:30:30 +0000

    [start]2-8

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -10,6 +10,16 @@ class TodolistsController < ApplicationController
     list.save
     redirect_to todolist_path(list.id)
   end
+  
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
+  
+  end
 
   def index
     @lists = List.all
--- sample_app/app/models/list.rb
+++ sample_app/app/models/list.rb
@@ -1,4 +1,8 @@
 class List < ApplicationRecord
   attachment :image
   
+  validates :title, presence: true
+  validates :body, prasrnce: true
+  validates :image, prasrnce: true
+  
 end
